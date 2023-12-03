50b9796201d024d3bde35361fa20a6f807d35556
ch3rryl <cherryl@live.co.uk>
2023-12-03 00:04:20 +0000

    day 2 wwcode xmas challenge - array at index

---
+++ day2/Arrays.java
@@ -0,0 +1,9 @@
+package day2;
+
+// Write a Java program to retrieve an element (at a specified index) from a given array list.
+public class Arrays {
+    public static void main(String[] args) {
+        int[] arrayofInts = {2, 10,20, 5, 9};
+        System.out.println(arrayofInts[2]);
+    }
+}
